1e8ed074773563dcd03ec898a944fa180641e562
Guy Marshall <guymarshall9@gmail.com>
2022-09-08 20:37:27 +0100

    Add decimal for extra decimal places

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import math
+from decimal import Decimal, getcontext
+
 
 def main():
+    getcontext().prec = 100
+    
     iterations = int(input("Enter a positive integer: "))
 
     results = []
     for number in range(1, iterations + 1):
         # print(f"{number}: {(1 + (1/number)) ** number}")
-        results.append(f"{number}: {(1 + (1/number)) ** number}\n")
+        results.append(f"{number}: {(Decimal(1) + (Decimal(1)/Decimal(number))) ** Decimal(number)}\n")
     
     with open("output.txt", "w") as file:
         for result in results:
