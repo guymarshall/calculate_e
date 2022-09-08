a74bb4628447399d1e114b282435734bd3de16cb
Guy Marshall <guymarshall9@gmail.com>
2022-09-08 18:45:25 +0100

    Add second algorithm for calculating e

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
+import math
+
+
 iterations = int(input("Enter a positive integer: "))
 
-for number in range(1, iterations + 1):
+for number in range(0, iterations + 1):
     print(f"{number}: {(1 + (1/number)) ** number}")
+
+result = 0
+for number in range(0, iterations + 1):
+    result += 1/(math.factorial(number))
+    print(f"{number}: {result}")
