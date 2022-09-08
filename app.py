c4de5d6cd5a4250f513e9f324e88e48d3e8192e5
Guy Marshall <guymarshall9@gmail.com>
2022-09-08 20:38:31 +0100

    Add messages for progress

--- app.py
+++ app.py
@@ -9,12 +9,15 @@ def main():
 
     results = []
     for number in range(1, iterations + 1):
-        # print(f"{number}: {(1 + (1/number)) ** number}")
         results.append(f"{number}: {(Decimal(1) + (Decimal(1)/Decimal(number))) ** Decimal(number)}\n")
+    
+    print("Writing to file...")
     
     with open("output.txt", "w") as file:
         for result in results:
             file.write(result)
+    
+    print("File write complete.")
 
 if __name__ == "__main__":
     main()
