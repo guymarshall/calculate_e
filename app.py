643091591c4c259aa169384beced20f6acd74a60
Guy Marshall <guymarshall9@gmail.com>
2022-09-08 18:25:16 +0100

    Add iterations input

--- app.py
+++ app.py
@@ -1,2 +1,4 @@
-for number in range(1, 1000000):
+iterations = int(input("Enter a positive integer: "))
+
+for number in range(1, iterations + 1):
     print(f"{number}: {(1 + (1/number)) ** number}")
